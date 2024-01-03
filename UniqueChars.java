22776b981f0b1b53beeaa92171c250f332e9ab79
yuvalkleinn <yuval.klein01@runi.post.ac.il>
2024-01-03 22:10:57 +0200

    Init UniqueChars

--- UniqueChars.java
+++ UniqueChars.java
@@ -1,7 +1,8 @@
 /** String processing exercise 2. */
 public class UniqueChars {
     public static void main(String[] args) {  
-        String str = args[0];
+        // String str = args[0];
+        String str = "Hello World";
         System.out.println(uniqueChars(str));
     }
 
@@ -12,6 +13,17 @@ public class UniqueChars {
      */
     public static String uniqueChars(String s) {
         // Replace the following statement with your code
-        return null;
+        String newStr = "";
+        int length = s.length();
+        for (int i = 0; i < length; i++){
+            char currentChar = s.charAt(i);
+            if (newStr.indexOf(currentChar)  == -1) {
+                newStr = newStr + s.charAt(i);
+            }
+            else if (s.charAt(i) == ' ') {
+                newStr = newStr + s.charAt(i);
+            }
+        }
+        return newStr;
     }
 }
